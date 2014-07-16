616c5aa123b70f43a7b76934b7af1f0508e3bfdd
Andrey Drebot <a.drebot@gmail.com>
2014-07-17 00:47:20 +0300

    Project was moved to GitHub

---
+++ app/models/course.js
@@ -0,0 +1,90 @@
+﻿define(['eventManager', 'guard', 'eventDataBuilders/courseEventDataBuilder', 'plugins/http'],
+    function (eventManager, guard, eventDataBuilder, http) {
+
+        function Course(spec) {
+            this.id = spec.id;
+            this.title = spec.title;
+            this.hasIntroductionContent = spec.hasIntroductionContent;
+            this.content = null;
+            this.objectives = spec.objectives;
+            this.score = spec.score;
+            this.isAnswered = isAnswered;
+            this.allQuestions = getAllQuestions(spec.objectives);
+            this.finish = finish;
+            this.calculateScore = calculateScore;
+            this.start = start;
+            this.loadContent = loadContent;
+            this.isCompleted = false;
+        }
+
+        function getAllQuestions(objectives) {
+            var questionsList = [];
+
+            _.each(objectives, function (objective) {
+                questionsList = questionsList.concat(objective.questions);
+            });
+
+            return _.shuffle(questionsList);
+        }
+
+        var finish = function (callback) {
+            eventManager.courseFinished(
+                eventDataBuilder.buildCourseFinishedEventData(this), function () {
+                    eventManager.turnAllEventsOff();
+                    callback();
+                });
+        };
+
+        var isAnswered = function () {
+            var notAnsweredQuestion = _.filter(this.allQuestions, function (question) {
+                return !question.isAnswered;
+            });
+
+            return notAnsweredQuestion.length === 0;
+        };
+
+        var start = function() {
+            this.score = 0;
+            eventManager.courseStarted();
+        };
+
+        var calculateScore = function () {
+            var result = _.reduce(this.objectives, function (memo, objective) {
+                objective.calculateScore();
+                return memo + objective.score;
+            }, 0);
+
+            var objectivesLength = this.objectives.length;
+            if (objectivesLength > 0) {
+                this.score = Math.floor(result / objectivesLength);
+                this.isCompleted = !_.some(this.objectives, function (objective) {
+                    return !objective.isCompleted;
+                });
+            } else {
+                this.score = 0;
+                this.isCompleted = false;
+            }
+        };
+
+        var loadContent = function () {
+            var that = this;
+            return Q.fcall(function () {
+                if (!that.hasIntroductionContent) {
+                    return null;
+                }
+
+                return http.get('content/content.html')
+                    .then(function (response) {
+                        that.content = response;
+                        return that.content;
+                    })
+                    .fail(function () {
+                        that.content = null;
+                        return that.content;
+                    });
+            });
+        };
+
+        return Course;
+    }
+);
---
+++ app/viewmodels/questions.js
@@ -0,0 +1,98 @@
+﻿define(['durandal/app', 'plugins/router', 'eventManager', 'configuration/settings', 'repositories/questionRepository', 'repositories/courseRepository', 'constants',
+    './questions/multipleSelectQuestion', './questions/singleSelectTextQuestion', './questions/fillInTheBlanksQuestion', './questions/dragAndDropTextQuestion',
+    'models/questions/multipleSelectQuestion', 'models/questions/singleSelectTextQuestion', 'models/questions/fillInTheBlanksQuestion', 'models/questions/dragAndDropTextQuestion'],
+    function (app, router, eventManager, settings, questionRepository, courseRepository, constants,
+        MultipleSelectQuestionViewModel, SingleSelectTextQuestionViewModel, FillInTheBlanksViewModel, DragAndDropTextQuestionViewModel,
+        MultipleSelectQuestionModel, SingleSelectTextQuestionModel, FillInTheBlanksQuestionModel, DragAndDropTextQuestionModel) {
+        "use strict";
+
+        var viewModel = {
+            courseTitle: '',
+            totalQuestionsCount: 0,
+            loadedQuestionsCount: 0,
+            questions: ko.observableArray([]),
+            isFullyLoaded: ko.observable(true),
+
+            canActivate: canActivate,
+            activate: activate,
+            loadQuestions: loadQuestions,
+            submit: submit
+        };
+
+        return viewModel;
+
+        function canActivate() {
+            var course = courseRepository.get();
+            return !course.isAnswered();
+        }
+
+        function activate() {
+            return Q.fcall(function () {
+                var course = courseRepository.get();
+                if (course == null) {
+                    router.navigate('404');
+                    return;
+                }
+
+                viewModel.courseTitle = course.title;
+                viewModel.totalQuestionsCount = course.allQuestions.length;
+
+                var mappedQuestions = _.chain(course.allQuestions).map(function (question, key) {
+                    if (question instanceof MultipleSelectQuestionModel) {
+                        return new MultipleSelectQuestionViewModel(question, key, course.allQuestions.length);
+                    } else if (question instanceof SingleSelectTextQuestionModel) {
+                        return new SingleSelectTextQuestionViewModel(question, key, course.allQuestions.length);
+                    } else if (question instanceof FillInTheBlanksQuestionModel) {
+                        return new FillInTheBlanksViewModel(question, key, course.allQuestions.length);
+                    } else if (question instanceof DragAndDropTextQuestionModel) {
+                        return new DragAndDropTextQuestionViewModel(question, key, course.allQuestions.length);
+                    }
+                }).filter(function (question) {
+                    return !_.isNullOrUndefined(question);
+                }).value();
+
+                viewModel.questions(mappedQuestions);
+            });
+        }
+
+        function submit() {
+            _.each(viewModel.questions(), function (question) {
+                question.submit();
+            });
+            router.navigate('summary');
+        }
+
+        function loadQuestions() {
+            var questionsToLoadContent = [],
+                questionsToLoadCount = loadedQuestionsCount + settings.loadingQuestionsInStepCount;
+
+            for (var i = loadedQuestionsCount; i < questionsToLoadCount; i++) {
+                if (i > allQuestions.length - 1) {
+                    isFullyLoaded(true);
+                    break;
+                }
+
+                questionsToLoadContent.push(allQuestions[i]);
+                loadedQuestionsCount++;
+            }
+
+            return questionRepository.loadQuestionContentCollection(questionsToLoadContent).then(function (questions) {
+                var mappedQuestions = _.chain(questions).map(function (question, key) {
+                    if (question instanceof MultipleSelectQuestionModel) {
+                        return new MultipleSelectQuestionViewModel(question, key, course.allQuestions.length);
+                    } else if (question instanceof SingleSelectTextQuestionModel) {
+                        return new SingleSelectTextQuestionViewModel(question, key, course.allQuestions.length);
+                    } else if (question instanceof FillInTheBlanksQuestionModel) {
+                        return new FillInTheBlanksViewModel(question, key, course.allQuestions.length);
+                    } else if (question instanceof DragAndDropTextQuestionModel) {
+                        return new DragAndDropTextQuestionViewModel(question, key, course.allQuestions.length);
+                    }
+                }).filter(function (question) {
+                    return !_.isNullOrUndefined(question);
+                }).value();
+
+                viewModel.questions.push(mappedQuestions);
+            });
+        }
+    }
+);
