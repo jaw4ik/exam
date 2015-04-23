530ccd7dd116e6cb18b280eacc3e1a9f87bf85af
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2015-04-23 15:36:11 +0300

    EWD-24703 - Extend reports on Results step: Fix xApi reporting to send correct object.id for objective statement. object.id for objective and parent.object.id for question should be equal.

--- app/xApi/activityProvider.js
+++ app/xApi/activityProvider.js
@@ -342,7 +342,7 @@
         }
 
         function createActivityForObjective(objectiveId, objectiveTitle) {
-            var activityId = activityProvider.rootActivityUrl + '?objectiveid=' + objectiveId;
+            var activityId = activityProvider.rootActivityUrl + '#objectives?objective_id=' + objectiveId;
             return createActivity(objectiveTitle, activityId);
         }
     }
