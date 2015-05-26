f4d0ca31dbe4df9360a4f1785b488ef69d32f4d3
Dmitry Zaets <dmitry.zaets@gmail.com>
2015-05-26 13:39:33 +0300

    Added token-auth support

--- settings/api.js
+++ settings/api.js
@@ -9,7 +9,15 @@
         settingsUrl = baseUrl + '/api/course/' + getURLParameter('courseId') + '/template/' + getURLParameter('templateId'),
 
         templateUrl = location.toString().substring(0, location.toString().indexOf('/settings/')) + '/',
-        manifestUrl = templateUrl + 'manifest.json';//TODO: Change way of resolving manifest file path
+        manifestUrl = templateUrl + 'manifest.json'; //TODO: Change way of resolving manifest file path
+
+
+    //  token auth support;
+    var token = localStorage['token.settings'];
+    var isTokenAuthSupported = token !== undefined;
+    var headers = isTokenAuthSupported ? { 'Authorization': 'Bearer ' + token } : {};
+    identifyUrl = isTokenAuthSupported ? baseUrl + '/auth/identity' : identifyUrl;
+    //  token auth support;
 
     window.egApi = {
         init: init,
@@ -21,15 +29,16 @@
     };
 
     function init() {
-        /* DEBUG */
+        /* DEBUG 
         var userDataPromise = $.Deferred().resolve([{ subscription: { accessType: 1, expirationDate: new Date(2016, 1, 1) } }]);
         var settingsPromise = $.getJSON('../../settings.js').then(function (response) { return [{ settings: JSON.stringify(response) }]; });
         var manifestPromise = $.getJSON(manifestUrl);
-        /* END_DEBUG */
+        END_DEBUG */
 
-        /* RELEASE
+        /* RELEASE */
         var userDataPromise = $.ajax({
             url: identifyUrl,
+            headers: headers,
             cache: false,
             type: 'POST',
             contentType: 'application/json',
@@ -38,6 +47,7 @@
 
         var settingsPromise = $.ajax({
             url: settingsUrl,
+            headers: headers,
             cache: false,
             contentType: 'application/json',
             dataType: 'json'
@@ -45,11 +55,12 @@
 
         var manifestPromise = $.ajax({
             url: manifestUrl,
+            headers: headers,
             cache: false,
             contentType: 'application/json',
             dataType: 'json'
         });
-        END_RELEASE */
+        /* END_RELEASE */
 
         return $.when(manifestPromise, userDataPromise, settingsPromise).done(function (manifestResponse, userDataResponse, settingsResponse) {
             apiData.manifest = getManifestModel(manifestResponse[0]);
@@ -91,13 +102,16 @@
     }
 
     function getUserModel(userData) {
+        //  token auth support;
+        userData = isTokenAuthSupported ? userData.data : userData;
+        //  token auth support;
         var user = { accessType: 0 };
         var starterAccessType = 1;
         if (userData.subscription &&
             userData.subscription.accessType &&
             userData.subscription.accessType >= starterAccessType &&
             new Date(userData.subscription.expirationDate) >= new Date()
-           ) {
+        ) {
             user.accessType = userData.subscription.accessType;
         }
         return user;
@@ -123,7 +137,6 @@
         return settings;
     }
 
-
     function getURLParameter(name) {
         return decodeURI(
             (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1]
@@ -133,16 +146,27 @@
     function saveSettings(settings, extraSettings, successSaveMessage, failSaveMessage) {
         freezeEditor();
 
-        return $.post(settingsUrl, { settings: settings, extraSettings: extraSettings })
-            .done(function () {
-                sendNotificationToEditor(successSaveMessage, true);
-            })
-            .fail(function () {
-                sendNotificationToEditor(failSaveMessage, false);
-            })
-            .always(function () {
-                unfreezeEditor();
-            });
+        var data = {
+            settings: settings,
+            extraSettings: extraSettings
+        };
+
+        var requestArgs = {
+            url: settingsUrl,
+            type: 'POST',
+            headers: headers,
+            cache: false,
+            dataType: 'json',
+            data: data
+        }
+
+        return $.ajax(requestArgs).done(function () {
+            sendNotificationToEditor(successSaveMessage, true);
+        }).fail(function () {
+            sendNotificationToEditor(failSaveMessage, false);
+        }).always(function () {
+            unfreezeEditor();
+        });
     }
 
     function freezeEditor() {
